88e779b506662260eb1501231679d38b0c7621ac
JonaMendez <jonatanmendez29@gmail.com>
2015-02-13 21:40:51 -0600

    Correccion de corrimiento que estaba en hexadecimal por como debe de ir, asi como la implementacion de Form 4 (Verificar caso 4 y 7). Hasta este commit falta implementar las banderas C y V de algunas instrucciones. En los casos 4 y 7 de la Form 4 ver cual es el carry bit. Del Form 5 ver que es el MSBd.

--- cCpu.cpp
+++ cCpu.cpp
@@ -66,82 +66,82 @@ void cCpu::executeOpcode()
 
 		if (opcode & 0xE000 == 0) { //Form 1 and 2
 			switch ((opcode & 0x1800) >> 11) {
-			case 0:
+			case 0: //LSL Rd,Rs,#
 				offset = (opcode >> 6) & 0x1F;
 				source = (opcode >> 3) & 0x7;
 				dest = opcode & 3;
 				Registers[dest] = Registers[source] << offset;
 				zFlag = (Registers[dest] == 0);
-				nFlag = ((Registers[dest] >> 0x80000000) == 1);
+				nFlag = ((Registers[dest] >> 30) == 1);
                                 cFlag = (); 
                                 vFlag = false;
 				cyclesCount += 1;
 				break;
-			case 1:
+			case 1: //LSR Rd,Rs,#
                                 offset = (opcode >> 6) & 0x1F;
                                 source = (opcode >> 3) & 0x7;
 				dest = opcode & 3;
                                 Registers[dest] = Registers[sourse] >> offset;
                                 zFlag = (Registers[dest] == 0);
-				nFlag = ((Registers[dest] >> 0x80000000) == 1);
+				nFlag = ((Registers[dest] >> 30) == 1);
                                 cFlag = (); 
                                 vFlag = false;
 				cyclesCount += 1;
 				break;
-			case 2:
+			case 2: //ASR Rd,Rs,#
                                 offset = (opcode >> 6) & 0x1F;
                                 source = (opcode >> 3) & 0x7;
                                 dest = opcode & 3;
                                 Registers[dest] = Registers[sourse] >> offset;
                                 zFlag = (Registers[dest] == 0);
-				nFlag = ((Registers[dest] >> 0x80000000) == 1);
+				nFlag = ((Registers[dest] >> 30) == 1);
                                 cFlag = (); 
                                 vFlag = false;
 				cyclesCount += 1;
 				break;
-			case 3:
+			case 3: //Form 2
                                 switch ((opcode & 0x600) >> 9){
-                                    case 0:
+                                    case 0: //ADD Rd,Rs,Rn
                                         operand = (opcode >> 6) & 0x7;
                                         source = (opcode >> 3) & 0x7;
                                         dest = opcode & 0x7;
                                         Registers[dest] = Registers[source] + Registers[operand];
                                         zFlag = (Registers[dest] == 0);
-                                        nFlag = ((Registers[dest] >> 0x80000000) == 1);
-                                        cFlag = (((Registers[operand] >> 0x80000000) & (Registers[source] >> 0x80000000)) == 1); //Check this
+                                        nFlag = ((Registers[dest] >> 30) == 1);
+                                        cFlag = (((Registers[operand] >> 30) & (Registers[source] >> 30)) == 1); //Check this
                                         vFlag = ((Registers[source] + Registers[operand]) == 0xFFFF);
                                         cyclesCount += 1;
                                         break;
-                                    case 1:
+                                    case 1: //SUB Rd,Rs,Rn
                                         operand = (opcode >> 6) & 0x7;
                                         source = (opcode >> 3) & 0x7;
                                         dest = opcode & 0x7;
                                         Registers[dest] = Registers[source] - Registers[operand];
                                         zFlag = (Registers[dest] == 0);
-                                        nFlag = ((Registers[dest] >> 0x80000000) == 1);
-                                        cFlag = (((Registers[operand] >> 0x80000000) & (Registers[source] >> 0x80000000)) == 1);
+                                        nFlag = ((Registers[dest] >> 30) == 1);
+                                        cFlag = (((Registers[operand] >> 30) & (Registers[source] >> 30)) == 1);
                                         vFlag = ((Registers[dest] = Registers[source] - Registers[operand]) == 0xFFFF);
                                         cyclesCount += 1;
                                         break;
-                                    case 2: 
+                                    case 2: //ADD Rd,Rs,# if #=0 MOV Rd,Rs
                                         operand = (opcode >> 6) & 0x7;
                                         source = (opcode >> 3) & 0x7;
                                         dest = opcode & 0x7;
                                         Registers[dest] = Registers[source] + operand;
                                         zFlag = (Registers[dest] == 0);
-                                        nFlag = ((Registers[dest] >> 0x80000000) == 1);
-                                        cFlag = (((Registers[dest] >> 0x80000000) & (Registers[source] >> 0x80000000)) == 0);
+                                        nFlag = ((Registers[dest] >> 30) == 1);
+                                        cFlag = (((Registers[dest] >> 30) & (Registers[source] >> 30)) == 0);
                                         vFlag = ();
                                         cyclesCount += 1;
                                         break;
-                                    case 3:
+                                    case 3: //SUB Rd,Rs,#
                                         operand = (opcode >> 6) & 0x7;
                                         source = (opcode >> 3) & 0x7;
                                         dest = opcode & 0x7;
                                         Registers[dest] = Registers[source] - operand;
                                         zFlag = (Registers[dest] == 0);
-                                        nFlag = ((Registers[dest] >> 0x80000000) == 1);
-                                        cFlag = (((Registers[dest] >> 0x80000000) & (Registers[source] >> 0x80000000)) == 0);
+                                        nFlag = ((Registers[dest] >> 30) == 1);
+                                        cFlag = (((Registers[dest] >> 30) & (Registers[source] >> 30)) == 0);
                                         vFlag = ();
                                         cyclesCount += 1;
                                         break;
@@ -151,15 +151,17 @@ void cCpu::executeOpcode()
 		}
                 if else (opcode & 0x2000 == 1){  //Form 3
                     switch ((opcode & 0x1800) >> 11){
-                        case 0:
+                        case 0: //MOV Rd,#
                             dest = (opcode >> 8) & 0x7;
                             operand = opcode & 0xFF;
                             Registers[dest] = operand;
                             zFlag = (Registers[dest] == 0);
-                            nFlag = ((Registers[dest] >> 0x80000000) == 1);
+                            nFlag = ((Registers[dest] >> 30) == 1);
+                            vFlag = false;
+                            cFlag = false;
                             cyclesCount += 1;
                             break;
-                        case 1:
+                        case 1: //CMP Rd,#
                             dest = (opcode >> 8) & 0x7;
                             operand = opcode & 0xFF;
                             zFlag = (( Registers[dest] - operand) == 0);
@@ -168,22 +170,22 @@ void cCpu::executeOpcode()
                             vFlag = ((( Registers[dest] - operand) & 0xFFFF) == 1);
                             cyclesCount += 1;
                             break;
-                        case 2:
+                        case 2: //ADD Rd,#
                             dest = (opcode >> 8) & 0x7;
                             operand = opcode & 0xFF;
                             Registers[dest] += operand;
                             zFlag = (Registers[dest] == 0);
-                            nFlag = ((Registers[dest] >> 0x80000000) == 1);
+                            nFlag = ((Registers[dest] >> 30) == 1);
                             cFlag = ();
                             vFlag = ();
                             cyclesCount += 1;
                             break;
-                        case 3:
+                        case 3: //SUB Rd,#
                             dest = (opcode >> 8) & 0x7;
                             operand = opcode & 0xFF;
                             Registers[dest] -= operand;
                             zFlag = (Registers[dest] == 0);
-                            nFlag = ((Registers[dest] >> 0x80000000) == 1);
+                            nFlag = ((Registers[dest] >> 30) == 1);
                             cFlag = ();
                             vFlag = ();
                             cyclesCount += 1;
@@ -199,25 +201,27 @@ void cCpu::executeOpcode()
                                      dest = (opcode &0x7);
                                      Registers[dest] = Registers[sourse] & Registers[dest];
                                      zFlag = (Registers[dest] == 0);
-                                     nFlag = ((Registers[dest] >> 0x80000000) == 1);
-                                     cFlag = ();
-                                     vFlag = ();
+                                     nFlag = ((Registers[dest] >> 30) == 1);
+                                     cFlag = false;
+                                     vFlag = false;
+                                     cyclesCount += 1;
                                      break;
                                  case 1: //EOX Rd,Rs
                                      sourse = (opcode & 0x38) >> 2;
                                      dest = (opcode &0x7);
                                      Registers[dest] = Registers[sourse] ^ Registers[dest];
                                      zFlag = (Registers[dest] == 0);
-                                     nFlag = ((Registers[dest] >> 0x80000000) == 1);
-                                     cFlag = ();
-                                     vFlag = ();
+                                     nFlag = ((Registers[dest] >> 30) == 1);
+                                     cFlag = false;
+                                     vFlag = false;
+                                     cyclesCount += 1;
                                      break;
                                  case 2: //LSL Rd,Rs
                                      sourse = (opcode & 0x38) >> 2;
                                      dest = (opcode &0x7);
                                      Registers[dest] = Registers[dest] << (Registers[sourse] & 0xFF);
                                      zFlag = (Registers[dest] == 0);
-                                     nFlag = ((Registers[dest] >> 0x80000000) == 1);
+                                     nFlag = ((Registers[dest] >> 30) == 1);
                                      cFlag = ();
                                      vFlag = false;
                                      break;
@@ -226,7 +230,7 @@ void cCpu::executeOpcode()
                                      dest = (opcode &0x7);
                                      Registers[dest] = Registers[dest] >> (Registers[sourse] & 0xFF);
                                      zFlag = (Registers[dest] == 0);
-                                     nFlag = ((Registers[dest] >> 0x80000000) == 1);
+                                     nFlag = ((Registers[dest] >> 30) == 1);
                                      cFlag = ();
                                      vFlag = false;
                                      break;
@@ -244,34 +248,120 @@ void cCpu::executeOpcode()
                                      dest = (opcode &0x7);
                                      Registers[dest] = Registers[dest] + Registers[sourse];
                                      zFlag = (Registers[dest] == 0);
-                                     nFlag = ((Registers[dest] >> 0x80000000) == 1);
+                                     nFlag = ((Registers[dest] >> 30) == 1);
                                      cFlag = ();
                                      vFlag = ();
+                                     cyclesCount += 1;
                                      break;
-                                 case 6:
+                                 case 6: //SBC Rd,Rs Check how sub carry bit
+                                     sourse = (opcode & 0x38) >> 2;
+                                     dest = (opcode &0x7);
+                                     Registers[dest] = Registers[dest] - Registers[sourse];
+                                     zFlag = (Registers[dest] == 0);
+                                     nFlag = ((Registers[dest] >> 30) == 1);
+                                     cFlag = ();
+                                     vFlag = ();
+                                     cyclesCount += 1;
                                      break;
-                                 case 7:
+                                 case 7: //ROR Rd,Rs
+                                     sourse = (opcode & 0x38) >> 2;
+                                     dest = (opcode &0x7);
+                                     Registers[dest] = Registers[dest] << (Registers[sourse] & 0xFF); //Check this
+                                     zFlag = (Registers[dest] == 0);
+                                     nFlag = ((Registers[dest] >> 30) == 1);
+                                     cFlag = ();
+                                     vFlag = false;
                                      break;
-                                 case 8:
+                                 case 8: //TST Rd,Rs
+                                     sourse = (opcode & 0x38) >> 2;
+                                     dest = (opcode &0x7);
+                                     zFlag = ((Registers[dest] & Registers[sourse]) == 0);
+                                     nFlag = (((Registers[dest] & Registers[sourse]) >> 30) == 1);
+                                     cFlag = false;
+                                     vFlag = false;
+                                     cyclesCount += 1;
                                      break;
-                                 case 9:
+                                 case 9: //NEG Rd,Rs
+                                     sourse = (opcode & 0x38) >> 2;
+                                     dest = (opcode &0x7);
+                                     Registers[dest] = 0 - Registers[sourse];
+                                     zFlag = (Registers[dest] == 0);
+                                     nFlag = ((Registers[dest] >> 30) == 1);
+                                     cFlag = ();
+                                     vFlag = ();
+                                     cyclesCount += 1;
                                      break;
-                                 case 0xA:
+                                 case 0xA: //CMP Rd,Rs
+                                     sourse = (opcode & 0x38) >> 2;
+                                     dest = (opcode &0x7);
+                                     zFlag = ((Registers[dest] - Registers[sourse]) == 0);
+                                     nFlag = (((Registers[dest] - Registers[sourse]) >> 30) == 1);
+                                     cFlag = ();
+                                     vFlag = ();
+                                     cyclesCount += 1;
                                      break;
-                                 case 0xB:
+                                 case 0xB: //CMN Rd,Rs
+                                     sourse = (opcode & 0x38) >> 2;
+                                     dest = (opcode &0x7);
+                                     zFlag = ((Registers[dest] + Registers[sourse]) == 0);
+                                     nFlag = (((Registers[dest] + Registers[sourse]) >> 30) == 1);
+                                     cFlag = ();
+                                     vFlag = ();
+                                     cyclesCount += 1;
                                      break;
-                                 case 0xC:
+                                 case 0xC: //ORR Rd,Rs
+                                     sourse = (opcode & 0x38) >> 2;
+                                     dest = (opcode &0x7);
+                                     Registers[dest] = Registers[dest] | Registers[sourse];
+                                     zFlag = (Registers[dest] == 0);
+                                     nFlag = (((Registers[dest]) >> 30) == 1);
+                                     cFlag = false;
+                                     vFlag = false;
+                                     cyclesCount += 1;
                                      break;
-                                 case 0xD:
+                                 case 0xD: //MUL Rd,Rs
+                                     sourse = (opcode & 0x38) >> 2;
+                                     dest = (opcode &0x7);
+                                     Registers[dest] = Registers[dest] * Registers[sourse];
+                                     zFlag = (Registers[dest] == 0);
+                                     nFlag = (((Registers[dest]) >> 30) == 1);
+                                     cFlag = ();
+                                     vFlag = false;
                                      break;
-                                 case 0xE:
+                                 case 0xE: //BIC Rd,Rs
+                                     sourse = (opcode & 0x38) >> 2;
+                                     dest = (opcode &0x7);
+                                     Registers[dest] = Registers[dest] & !(Registers[sourse]);
+                                     zFlag = (Registers[dest] == 0);
+                                     nFlag = (((Registers[dest]) >> 30) == 1);
+                                     cFlag = false;
+                                     vFlag = false;
+                                     cyclesCount += 1;
                                      break;
-                                 case 0xF:
+                                 case 0xF: //MVN Rd,RS
+                                     sourse = (opcode & 0x38) >> 2;
+                                     dest = (opcode &0x7);
+                                     Registers[dest] = !(Registers[sourse]);
+                                     zFlag = (Registers[dest] == 0);
+                                     nFlag = (((Registers[dest]) >> 30) == 1);
+                                     cFlag = false;
+                                     vFlag = false;
+                                     cyclesCount += 1;
                                      break;
                              }
                             break;
                         case 1: //Form 5
-                            
+                            switch ((opcode & 0x300) >> 9){
+                                case 0: //ADD Rd,Rs
+                                    
+                                    break;
+                                case 1:
+                                    break;
+                                case 2:
+                                    break;
+                                case 3:
+                                    break;
+                            }
                             break;
                     }
                     
