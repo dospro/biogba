ae2381154648d1bb554adaa007b8445a4952c594
dospro <dospro@gmail.com>
2023-07-09 12:20:26 -0600

    Refactored assembler code
    * Moved FSM into iterator form. V has a bug which won't let me use custom iterators correctly
    * The iterator also forced me to use options instead of results which may not be that nice.

--- src/biogba/assembler.v
+++ src/biogba/assembler.v
@@ -1,6 +1,6 @@
 module biogba
 
-// Function that parses a string into a OpcodeCondition
+// Parses a string into a OpcodeCondition
 fn condition_from_string(condition_string string) ?OpcodeCondition {
 	return match condition_string.to_lower() {
 		'eq' { OpcodeCondition.eq }
@@ -154,336 +154,417 @@ fn (mut iter OpcodeNameParser) next() ?string {
 	return iter.token
 }
 
+struct OpcodeParser {
+mut:
+	scanner OpcodeNameParser
+	errors  []IError
+	fields  []string
+	state   int
+}
+
+fn new_opcode_parser(opcode_text string) !OpcodeParser {
+	// Remove commas
+	cleaned_opcode_text := opcode_text.replace(',', ' ')
+
+	// Separate tokens
+	fields := cleaned_opcode_text.fields()
+	my_scanner := OpcodeNameParser{
+		opcode_string: fields[0]
+	}
+
+	return OpcodeParser{
+		scanner: my_scanner
+		fields: fields
+		state: 0
+	}
+}
+
+fn (mut iter OpcodeParser) next() ?OpcodeToken {
+	match iter.state {
+		0 {
+			token := iter.scanner.next() or {
+				iter.errors << error('Invalid Opcode name')
+				return none
+			}
+			iter.state = 1
+			return OpcodeToken{
+				token_value: token
+				token_type: TokenType.opcode_name
+			}
+		}
+		1 {
+			// We have 3 cases:
+			// 1. Condition string
+			// 2. S flag
+			// 3. register
+
+			// If the internal scanner is out of characters, then
+			// the next token is a register and is inside the next
+			// tokens element
+			token := iter.scanner.next()
+
+			match token {
+				none {
+					iter.state = 4
+					return OpcodeToken{
+						token_value: register_number_from_string(iter.fields[1]) or {
+							iter.errors << err
+							return none
+						}
+						token_type: TokenType.register
+					}
+				}
+				'S' {
+					iter.state = 3
+					return OpcodeToken{
+						token_value: true
+						token_type: TokenType.s_bit
+					}
+				}
+				else {
+					iter.state = 2
+					return OpcodeToken{
+						token_value: condition_from_string(token or { '' }) or {
+							OpcodeCondition.al
+						}
+						token_type: TokenType.condition
+					}
+				}
+			}
+		}
+		2 {
+			// After a condition we can have
+			// 1. An S flag
+			// 2. A register
+
+			// If the internal scanner is our of characters, then
+			// the next token is a register and is inside the next
+			// tokens element
+			token := iter.scanner.next()
+
+			match token {
+				none {
+					iter.state = 4
+					return OpcodeToken{
+						token_value: register_number_from_string(iter.fields[1]) or {
+							iter.errors << err
+							return none
+						}
+						token_type: TokenType.register
+					}
+				}
+				'S' {
+					iter.state = 3
+					return OpcodeToken{
+						token_value: true
+						token_type: TokenType.s_bit
+					}
+				}
+				else {
+					iter.errors << error('After opcode name there was no S, register or condition. Mal formed opcode')
+					return none
+				}
+			}
+		}
+		3 {
+			// After an S flag we can only have a register
+			iter.state = 4
+			return OpcodeToken{
+				token_value: register_number_from_string(iter.fields[1]) or {
+					iter.errors << err
+					return none
+				}
+				token_type: TokenType.register
+			}
+		}
+		4 {
+			// After the first register we can only have a second register
+			iter.state = 5
+			return OpcodeToken{
+				token_value: register_number_from_string(iter.fields[2]) or {
+					iter.errors << err
+					return none
+				}
+				token_type: TokenType.register
+			}
+		}
+		5 {
+			// After the second register we can have:
+			// 1. An expression
+			// 2. A register
+
+			// If the token starts with R, then it is a register
+			if iter.fields[3].substr(0, 1) == 'R' {
+				iter.state = 6
+				return OpcodeToken{
+					token_value: register_number_from_string(iter.fields[3]) or {
+						iter.errors << err
+						return none
+					}
+					token_type: TokenType.register
+				}
+			} else {
+				// Otherwise, it is an expression
+				if iter.fields.len > 4 {
+					iter.errors << error('Too many parameters')
+					return none
+				}
+				expression_value := u32(iter.fields[3][1..].parse_uint(16, 32) or {
+					iter.errors << err
+					return none
+				})
+				iter.state = 11
+				return OpcodeToken{
+					token_value: expression_value
+					token_type: TokenType.expression
+				}
+			}
+		}
+		6 {
+			// After a 3th register we are now in register mode.
+			// So we either hace RXX or shiftname
+			if iter.fields[4].to_lower() == 'rxx' {
+				if iter.fields.len > 5 {
+					iter.errors << error('Too many parameters')
+					return none
+				}
+				iter.state = 10
+				return OpcodeToken{
+					token_value: shift_type_from_string('ror') or {
+						iter.errors << err
+						return none
+					}
+					token_type: TokenType.shift_name
+				}
+			} else {
+				iter.state = 7
+				return OpcodeToken{
+					token_value: shift_type_from_string(iter.fields[4]) or {
+						iter.errors << err
+						return none
+					}
+					token_type: TokenType.shift_name
+				}
+			}
+		}
+		7 {
+			// After the shift name we can have:
+			// 1. Expression (Shift value)
+			// 2. Register
+			if iter.fields[5].substr(0, 1).to_lower() == 'r' {
+				if iter.fields.len > 6 {
+					iter.errors << error('Too many parameters')
+					return none
+				}
+				iter.state = 8
+				return OpcodeToken{
+					token_value: register_number_from_string(iter.fields[5]) or {
+						iter.errors << err
+						return none
+					}
+					token_type: TokenType.register
+				}
+			} else {
+				if iter.fields.len > 6 {
+					iter.errors << error('Too many parameters')
+					return none
+				}
+				iter.state = 9
+				return OpcodeToken{
+					token_value: u8(iter.fields[5][1..].parse_uint(16, 8) or {
+						iter.errors << err
+						return none
+					})
+					token_type: TokenType.expression
+				}
+			}
+		}
+		else {
+			iter.errors << error('Invalid state in general parser')
+			return none
+		}
+	}
+}
+
+fn (self OpcodeParser) get_final_state() !int {
+	final_states := [8, 9, 10, 11]
+	if final_states.contains(self.state) {
+		return self.state
+	}
+	return error('State ${self.state} is not final. Something went wrong')
+}
+
+struct TokensList {
+	state  int
+	tokens []OpcodeToken
+}
+
+/*
+Takes the initial text representation of an opcode and returns a
+TokensList struct with the final state (which identifies the actual opcode)
+together with the list of validated tokens that builds the opcode.
+*/
+fn tokens_from_string(opcode_text string) !TokensList {
+	mut opcode_parser := new_opcode_parser(opcode_text)!
+	mut tokens_list := []OpcodeToken{}
+
+	for {
+		token := opcode_parser.next() or { break }
+		tokens_list << token
+	}
+	if state := opcode_parser.get_final_state() {
+		return TokensList{
+			state: state
+			tokens: tokens_list
+		}
+	} else {
+		return err
+	}
+}
+
 /*
 Returns a Result type with either an Opcode struct from a string
 representation or an error in case something goes wrong
 */
 fn opcode_from_string(opcode_text string) !Opcode {
-	// Remove commas	
-	cleaned_opcode_text := opcode_text.replace(',', ' ')
+	parsed_tokens := tokens_from_string(opcode_text)!
+	tokens_list := parsed_tokens.tokens
+	general_state := parsed_tokens.state
 
-	// Separate tokens
-	tokens := cleaned_opcode_text.fields()
+	mut condition := OpcodeCondition.al
+	mut s_bit := false
+	mut rd := u8(0x0)
+	mut rn := u8(0x0)
+	mut rm := u8(0x0)
+	mut immediate_value := u32(0x0)
 
-	mut general_state := 0
-	mut real_token := OpcodeToken{}
-	mut tokens_list := []OpcodeToken{}
-	println('Token0 ${tokens[0]}')
-	mut my_scanner := OpcodeNameParser{
-		opcode_string: tokens[0]
+	mut current_token := 1
+	if tokens_list[current_token].token_type == TokenType.condition {
+		condition = tokens_list[1].token_value as OpcodeCondition
+		current_token += 1
+	} else {
+		condition = OpcodeCondition.al
+	}
+	if tokens_list[current_token].token_type == TokenType.s_bit {
+		s_bit = true
+		current_token += 1
+	} else {
+		s_bit = false
+	}
+	if tokens_list[current_token].token_type == TokenType.register {
+		rd = tokens_list[current_token].token_value as u8
+		current_token += 1
+		println('Rd ${rd}')
+	} else {
+		return error('Expected register Rd')
+	}
+	if tokens_list[current_token].token_type == TokenType.register {
+		rn = tokens_list[current_token].token_value as u8
+		println('Rn ${rn}')
+		current_token += 1
+	} else {
+		error('Expected register Rn')
 	}
 
-	for {
-		println('General state ${general_state}')
-		match general_state {
-			0 {
-				token := my_scanner.next() or { return error('Invalid opcode name') }
-				println('Opcode Name ${token}')
+	match general_state {
+		8 {
+			rm = tokens_list[current_token].token_value as u8
+			current_token += 1
+			println('Rm ${rm}')
+			shift_type := tokens_list[current_token].token_value as ShiftType
+			current_token += 1
+			rs := tokens_list[current_token].token_value as u8
 
-				general_state = 1
-				real_token = OpcodeToken{
-					token_value: token
-					token_type: TokenType.opcode_name
-				}
-			}
-			1 {
-				// We have 3 cases:
-				// 1. Condition string
-				// 2. S flag
-				// 3. register
+			opcode_name := tokens_list[0].token_value as string
 
-				// If the internal scanner is out of characters, then
-				// the next token is a register and is inside the next
-				// tokens element
-				token := my_scanner.next()
-
-				match token {
-					none {
-						general_state = 4
-						real_token = OpcodeToken{
-							token_value: register_number_from_string(tokens[1])!
-							token_type: TokenType.register
-						}
-					}
-					'S' {
-						general_state = 3
-						real_token = OpcodeToken{
-							token_value: true
-							token_type: TokenType.s_bit
-						}
-					}
-					else {
-						general_state = 2
-						real_token = OpcodeToken{
-							token_value: condition_from_string(token or { '' }) or {
-								OpcodeCondition.al
-							}
-							token_type: TokenType.condition
-						}
+			if opcode_name == 'ADD' {
+				return ADDOpcode{
+					condition: condition
+					rd: rd
+					rn: rn
+					s_bit: s_bit
+					shift_operand: ShiftOperandRegister{
+						rm: rm
+						register_shift: true
+						shift_type: shift_type
+						shift_value: rs
 					}
 				}
-			}
-			2 {
-				// After a condition we can have
-				// 1. An S flag
-				// 2. A register
-
-				// If the internal scanner is our of characters, then
-				// the next token is a register and is inside the next
-				// tokens element
-				token := my_scanner.next()
-
-				match token {
-					none {
-						general_state = 4
-						real_token = OpcodeToken{
-							token_value: register_number_from_string(tokens[1])!
-							token_type: TokenType.register
-						}
-					}
-					'S' {
-						general_state = 3
-						real_token = OpcodeToken{
-							token_value: true
-							token_type: TokenType.s_bit
-						}
-					}
-					else {
-						return error('After opcode name there was no S, register or condition. Mal formed opcode')
+			} else {
+				return ADCOpcode{
+					condition: condition
+					rd: rd
+					rn: rn
+					s_bit: s_bit
+					shift_operand: ShiftOperandRegister{
+						rm: rm
+						register_shift: true
+						shift_type: shift_type
+						shift_value: rs
 					}
 				}
-			}
-			3 {
-				// After an S flag we can only have a register
-				general_state = 4
-				real_token = OpcodeToken{
-					token_value: register_number_from_string(tokens[1])!
-					token_type: TokenType.register
-				}
-			}
-			4 {
-				// After the first register we can only have a second register
-				general_state = 5
-				real_token = OpcodeToken{
-					token_value: register_number_from_string(tokens[2])!
-					token_type: TokenType.register
-				}
-			}
-			5 {
-				// After the second register we can have:
-				// 1. An expression
-				// 2. A register
-
-				// If the token starts with R, then it is a register
-				if tokens[3].substr(0, 1) == 'R' {
-					println('Register ${tokens[3]}')
-					general_state = 6
-					real_token = OpcodeToken{
-						token_value: register_number_from_string(tokens[3])!
-						token_type: TokenType.register
-					}
-				} else {
-					// Otherwise, it is an expression
-					println('Expression ${tokens[3]}')
-					if tokens.len > 4 {
-						return error('Too many parameters')
-					}
-					general_state = 11
-					real_token = OpcodeToken{
-						token_value: u32(tokens[3][1..].parse_uint(16, 32)!)
-						token_type: TokenType.expression
-					}
-				}
-			}
-			6 {
-				// After a 3th register we are now in register mode.
-				// So we either hace RXX or shiftname
-				println('Shift Name ${tokens[4]}')
-				if tokens[4].to_lower() == 'rxx' {
-					if tokens.len > 5 {
-						return error('Too many parameters')
-					}
-					general_state = 10
-					real_token = OpcodeToken{
-						token_value: shift_type_from_string('ror')!
-						token_type: TokenType.shift_name
-					}
-				} else {
-					general_state = 7
-					real_token = OpcodeToken{
-						token_value: shift_type_from_string(tokens[4])!
-						token_type: TokenType.shift_name
-					}
-				}
-			}
-			7 {
-				// After the shift name we can have:
-				// 1. Expression (Shift value)
-				// 2. Register
-				println('Expression ${tokens[5]}')
-				if tokens[5].substr(0, 1).to_lower() == 'r' {
-					if tokens.len > 6 {
-						return error('Too many parameters')
-					}
-					general_state = 8
-					real_token = OpcodeToken{
-						token_value: register_number_from_string(tokens[5])!
-						token_type: TokenType.register
-					}
-				} else {
-					if tokens.len > 6 {
-						return error('Too many parameters')
-					}
-					general_state = 9
-					real_token = OpcodeToken{
-						token_value: u8(tokens[5][1..].parse_uint(16, 8)!)
-						token_type: TokenType.expression
-					}
-				}
-			}
-			else {
-				return error('Invalid state ${general_state}')
 			}
 		}
-		tokens_list << real_token
-		final_states := [8, 9, 10, 11]
-		if final_states.contains(general_state) {
-			mut condition := OpcodeCondition.al
-			mut s_bit := false
-			mut rd := u8(0x0)
-			mut rn := u8(0x0)
-			mut rm := u8(0x0)
-			mut immediate_value := u32(0x0)
-
-			mut current_token := 1
-			if tokens_list[current_token].token_type == TokenType.condition {
-				condition = tokens_list[1].token_value as OpcodeCondition
-				current_token += 1
+		9 {
+			rm = tokens_list[current_token].token_value as u8
+			current_token += 1
+			println('Rm ${rm}')
+			shift_type := tokens_list[current_token].token_value as ShiftType
+			current_token += 1
+			expression := tokens_list[current_token].token_value as u8
+			if expression > 0x1F {
+				return error('Shift expression too big')
+			}
+			return ADCOpcode{
+				condition: condition
+				rd: rd
+				rn: rn
+				s_bit: s_bit
+				shift_operand: ShiftOperandRegister{
+					rm: rm
+					register_shift: false
+					shift_type: shift_type
+					shift_value: expression
+				}
+			}
+		}
+		10 {
+			// In final state 10 we are parsing an RXX which
+			// is build as a ROR #0
+			rm = tokens_list[current_token].token_value as u8
+			current_token += 1
+			println('Rm ${rm}')
+			return ADCOpcode{
+				condition: condition
+				rd: rd
+				rn: rn
+				s_bit: s_bit
+				shift_operand: ShiftOperandRegister{
+					rm: rm
+					register_shift: false
+					shift_type: ShiftType.ror
+					shift_value: 0
+				}
+			}
+		}
+		11 {
+			if tokens_list[current_token].token_type == TokenType.expression {
+				immediate_value = tokens_list[current_token].token_value as u32
+				println('Immediate value ${immediate_value}')
 			} else {
-				condition = OpcodeCondition.al
+				return error('Invalid expression ${tokens_list[current_token].token_value}')
 			}
-			if tokens_list[current_token].token_type == TokenType.s_bit {
-				s_bit = true
-				current_token += 1
-			} else {
-				s_bit = false
+			shift_operand := get_immediate_value(immediate_value)!
+			return ADCOpcode{
+				condition: condition
+				rd: rd
+				rn: rn
+				s_bit: s_bit
+				shift_operand: shift_operand
 			}
-			if tokens_list[current_token].token_type == TokenType.register {
-				rd = tokens_list[current_token].token_value as u8
-				current_token += 1
-				println('Rd ${rd}')
-			} else {
-				return error('Expected register Rd')
-			}
-			if tokens_list[current_token].token_type == TokenType.register {
-				rn = tokens_list[current_token].token_value as u8
-				println('Rn ${rn}')
-				current_token += 1
-			} else {
-				error('Expected register Rn')
-			}
-
-			match general_state {
-				8 {
-					rm = tokens_list[current_token].token_value as u8
-					current_token += 1
-					println('Rm ${rm}')
-					shift_type := tokens_list[current_token].token_value as ShiftType
-					current_token += 1
-					rs := tokens_list[current_token].token_value as u8
-
-					opcode_name := tokens_list[0].token_value as string
-
-					if opcode_name == 'ADD' {
-						return ADDOpcode{
-							condition: condition
-							rd: rd
-							rn: rn
-							s_bit: s_bit
-							shift_operand: ShiftOperandRegister{
-								rm: rm
-								register_shift: true
-								shift_type: shift_type
-								shift_value: rs
-							}
-						}
-					} else {
-						return ADCOpcode{
-							condition: condition
-							rd: rd
-							rn: rn
-							s_bit: s_bit
-							shift_operand: ShiftOperandRegister{
-								rm: rm
-								register_shift: true
-								shift_type: shift_type
-								shift_value: rs
-							}
-						}
-					}
-				}
-				9 {
-					rm = tokens_list[current_token].token_value as u8
-					current_token += 1
-					println('Rm ${rm}')
-					shift_type := tokens_list[current_token].token_value as ShiftType
-					current_token += 1
-					expression := tokens_list[current_token].token_value as u8
-					if expression > 0x1F {
-						return error('Shift expression too big')
-					}
-					return ADCOpcode{
-						condition: condition
-						rd: rd
-						rn: rn
-						s_bit: s_bit
-						shift_operand: ShiftOperandRegister{
-							rm: rm
-							register_shift: false
-							shift_type: shift_type
-							shift_value: expression
-						}
-					}
-				}
-				10 {
-					// In final state 10 we are parsing an RXX which
-					// is build as a ROR #0
-					rm = tokens_list[current_token].token_value as u8
-					current_token += 1
-					println('Rm ${rm}')
-					return ADCOpcode{
-						condition: condition
-						rd: rd
-						rn: rn
-						s_bit: s_bit
-						shift_operand: ShiftOperandRegister{
-							rm: rm
-							register_shift: false
-							shift_type: ShiftType.ror
-							shift_value: 0
-						}
-					}
-				}
-				11 {
-					if tokens_list[current_token].token_type == TokenType.expression {
-						immediate_value = tokens_list[current_token].token_value as u32
-						println('Immediate value ${immediate_value}')
-					} else {
-						return error('Invalid expression ${tokens_list[current_token].token_value}')
-					}
-					shift_operand := get_immediate_value(immediate_value)!
-					return ADCOpcode{
-						condition: condition
-						rd: rd
-						rn: rn
-						s_bit: s_bit
-						shift_operand: shift_operand
-					}
-				}
-				else {
-					return error('Not implemented')
-				}
-			}
+		}
+		else {
+			return error('Not implemented')
 		}
 	}
 
